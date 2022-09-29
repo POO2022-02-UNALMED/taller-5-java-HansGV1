4b804fc79f13d4110e09c8898bf3e2788157f99d
Hans García <82074617+HansGV1@users.noreply.github.com>
2022-09-29 15:00:14 -0500

    Up

--- src/test/java/test/zooAnimales/Animal.java
+++ src/test/java/test/zooAnimales/Animal.java
@@ -33,6 +33,34 @@ public class Animal {
 			"Anfibios: "+(Anfibio.cantidadAnfibios()+1);
 	}
 
+	public class testTotalTipo{
+	public static void main(String[] args) {
+		new Anfibio();
+		new Anfibio();
+		new Mamifero();
+		new Mamifero();
+		new Mamifero();
+		new Reptil();
+		new Pez();
+		new Ave();
+		new Ave();
+		boolean ok = false;
+		String  comp = "Mamiferos: 4\n" + 
+			"Aves: 4\n" + 
+			"Reptiles: 2\n" + 
+			"Peces: 2\n" + 
+			"Anfibios: 3";
+		
+		if(Animal.totalPorTipo().equals(comp)) {
+			ok = true;
+			System.out.println(ok);
+		}
+		System.out.println(Animal.totalPorTipo());
+		System.out.println(" ");
+		System.out.println(comp);
+	}
+
+	
 	public String getNombre() {
 		
 		return this.nombre;
