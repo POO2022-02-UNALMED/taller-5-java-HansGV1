0d8e4bcdacaecddadd95d07ee7caf663a2479ce9
Hans García <82074617+HansGV1@users.noreply.github.com>
2022-09-29 14:34:32 -0500

    Up

--- src/test/java/test/zooAnimales/Mamifero.java
+++ src/test/java/test/zooAnimales/Mamifero.java
@@ -24,7 +24,7 @@ public class Mamifero extends Animal {
 		numMamiferos++;
 	}
 	public static int cantidadMamiferos() {
-		numMamiferos = numMamiferos + 1
+		numMamiferos = numMamiferos + 1;
 		return numMamiferos;
 	}
 	public static Animal  crearCaballo(String nombre, int edad, String genero) {
